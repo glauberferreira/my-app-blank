cc1cd3bf01de230a1d14a6d3fd09887ab2e922a7
Glauber Ferreira <glauber.ferreira@ifal.edu.br>
2024-10-14 14:23:39 -0300

    Ajustes na tela inicial

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-paper';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.texto}>Open up App.js to start working on your app!</Text>
+      <TextInput/>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +16,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    //alignItems: 'center',
     justifyContent: 'center',
   },
+  texto: {
+    color: '#12ab32'
+  }
 });
