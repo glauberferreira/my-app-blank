612e26e367d49021a8c48f0ecba05a302023ea88
Glauber Ferreira <glauber.ferreira@ifal.edu.br>
2024-10-14 15:58:33 -0300

    senha

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { TextInput } from 'react-native-paper';
 
 export default function App() {
+  const [senhaProtegida, setSenhaProtegida] = useState(true);
+
+  function inverter() {
+    setSenhaProtegida(!senhaProtegida);
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.texto}>Open up App.js to start working on your app!</Text>
-      <TextInput/>
+      <TextInput secureTextEntry={senhaProtegida} right={<TextInput.Icon icon="eye" onPress={() => inverter()} />}/>
+      <TextInput keyboardType='email-address'/>
       <StatusBar style="auto" />
     </View>
   );
