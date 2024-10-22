18ef95895606b743bad6103734b5c9cd661001e7
Glauber Ferreira <glauber.ferreira@ifal.edu.br>
2024-10-22 16:28:53 -0300

    cadastro via API

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
-import { TextInput } from 'react-native-paper';
+import { Button, TextInput } from 'react-native-paper';
 
 export default function App() {
-  const [senhaProtegida, setSenhaProtegida] = useState(true);
+  const [descricao, setDescricao] = useState('');
+  const [valor, setValor] = useState(0);
 
-  function inverter() {
-    setSenhaProtegida(!senhaProtegida);
+  const handleCadastrar = () => {
+    console.log(descricao);
+    console.log(valor);
+
+    fetch('https://668d6800099db4c579f2dcc2.mockapi.io/roupas', {
+      method: 'POST',
+      headers: {
+         Accept: 'application/json',
+         'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        valor: valor,
+        descricao: descricao,
+      }),
+    });    
   }
+  
+  // const [senhaProtegida, setSenhaProtegida] = useState(true);
+
+  // function inverter() {
+  //   setSenhaProtegida(!senhaProtegida);
+  // }
   
   return (
     <View style={styles.container}>
-      <Image source={require('./assets/icon.png')} style={styles.logotipo}/>
+      {/* <Image source={require('./assets/icon.png')} style={styles.logotipo}/>
       <Text style={styles.texto}>Open up App.js to start working on your app!</Text>
       <TextInput secureTextEntry={senhaProtegida} right={<TextInput.Icon icon="eye" onPress={() => inverter()} />}/>
-      <TextInput keyboardType='email-address' style={styles.entradaTexto} activeUnderlineColor='red' label='Nome Completo'/>
+      <TextInput keyboardType='email-address' style={styles.entradaTexto} activeUnderlineColor='red' label='Nome Completo'/> */}
+      <TextInput label="Descrição" defaultValue={descricao} onChangeText={setDescricao}/>
+      <TextInput label="Valor" defaultValue={valor} onChangeText={setValor}/>
+      <Button mode='contained' onPress={handleCadastrar}>Cadastrar</Button>
+      
       <StatusBar style="auto" />
     </View>
   );
