3389ed607af580bf789820e972e7de70d53b28e7
Glauber Ferreira <glauber.ferreira@ifal.edu.br>
2024-10-29 16:12:20 -0300

    adiciona tela de cadastro e navegação entre as telas

---
+++ app/cadastro.js
@@ -0,0 +1,69 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { Button, TextInput } from 'react-native-paper';
+
+export default function App() {
+  const [descricao, setDescricao] = useState('');
+  const [valor, setValor] = useState(0);
+  const [cadastrando, setCadastrando] = useState(false);
+
+  const handleCadastrar = async () => {
+    setCadastrando(true);
+
+    await fetch('https://668d6800099db4c579f2dcc2.mockapi.io/roupas', {
+      method: 'POST',
+      headers: {
+         Accept: 'application/json',
+         'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        valor: valor,
+        descricao: descricao,
+      }),
+    });
+
+    setCadastrando(false);
+  }
+  
+  // const [senhaProtegida, setSenhaProtegida] = useState(true);
+
+  // function inverter() {
+  //   setSenhaProtegida(!senhaProtegida);
+  // }
+  
+  return (
+    <View style={styles.container}>
+      <Image source={require('./../assets/icon.png')} style={styles.logotipo}/>
+      {/* <Text style={styles.texto}>Open up App.js to start working on your app!</Text>
+      <TextInput secureTextEntry={senhaProtegida} right={<TextInput.Icon icon="eye" onPress={() => inverter()} />}/>
+      <TextInput keyboardType='email-address' style={styles.entradaTexto} activeUnderlineColor='red' label='Nome Completo'/> */}
+      <TextInput label="Descrição" defaultValue={descricao} onChangeText={setDescricao}/>
+      <TextInput label="Valor" defaultValue={valor} onChangeText={setValor}/>
+      <Button mode='contained' onPress={handleCadastrar} loading={cadastrando} >Cadastrar</Button>
+      
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    //alignItems: 'center',
+    justifyContent: 'space-evenly',
+    paddingHorizontal: 20
+  },
+  texto: {
+    color: '#12ab32'
+  },
+  logotipo: {
+    width: 256,
+    height: 256,
+    alignSelf: 'center'
+  },
+  entradaTexto: {
+    backgroundColor: 'pink',
+  }
+});
--- app/index.js
+++ app/index.js
@@ -1,69 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
-import { Button, TextInput } from 'react-native-paper';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Link } from 'expo-router'
 
-export default function App() {
-  const [descricao, setDescricao] = useState('');
-  const [valor, setValor] = useState(0);
-  const [cadastrando, setCadastrando] = useState(false);
-
-  const handleCadastrar = async () => {
-    setCadastrando(true);
-
-    await fetch('https://668d6800099db4c579f2dcc2.mockapi.io/roupas', {
-      method: 'POST',
-      headers: {
-         Accept: 'application/json',
-         'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        valor: valor,
-        descricao: descricao,
-      }),
-    });
-
-    setCadastrando(false);
-  }
-  
-  // const [senhaProtegida, setSenhaProtegida] = useState(true);
-
-  // function inverter() {
-  //   setSenhaProtegida(!senhaProtegida);
-  // }
-  
+const Index = () => {
   return (
-    <View style={styles.container}>
-      <Image source={require('./../assets/icon.png')} style={styles.logotipo}/>
-      {/* <Text style={styles.texto}>Open up App.js to start working on your app!</Text>
-      <TextInput secureTextEntry={senhaProtegida} right={<TextInput.Icon icon="eye" onPress={() => inverter()} />}/>
-      <TextInput keyboardType='email-address' style={styles.entradaTexto} activeUnderlineColor='red' label='Nome Completo'/> */}
-      <TextInput label="Descrição" defaultValue={descricao} onChangeText={setDescricao}/>
-      <TextInput label="Valor" defaultValue={valor} onChangeText={setValor}/>
-      <Button mode='contained' onPress={handleCadastrar} loading={cadastrando} >Cadastrar</Button>
-      
-      <StatusBar style="auto" />
+    <View>
+      <Text>Index</Text>
+      <Link href='/cadastro'>Nova Roupa</Link>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'yellow',
-    //alignItems: 'center',
-    justifyContent: 'space-evenly',
-    paddingHorizontal: 20
-  },
-  texto: {
-    color: '#12ab32'
-  },
-  logotipo: {
-    width: 256,
-    height: 256,
-    alignSelf: 'center'
-  },
-  entradaTexto: {
-    backgroundColor: 'pink',
-  }
-});
+export default Index
+
+const styles = StyleSheet.create({})
