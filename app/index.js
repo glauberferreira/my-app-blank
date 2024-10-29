216eb27bfb350a609a7c93acf9fcb6e8ad4903a7
Glauber Ferreira <glauber.ferreira@ifal.edu.br>
2024-10-29 16:33:32 -0300

    Link + Button

--- app/index.js
+++ app/index.js
@@ -1,12 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
+import { Button } from 'react-native-paper'
 
 const Index = () => {
   return (
     <View>
       <Text>Index</Text>
       <Link href='/cadastro'>Nova Roupa</Link>
+      
+      <Link href='/cadastro' asChild>
+        <Button mode='contained'>Nova Roupa</Button>
+      </Link>
     </View>
   )
 }
