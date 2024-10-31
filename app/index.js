553c4b17b928356b8b4d7354ef8cddf003f83058
Glauber Ferreira <glauber.ferreira@ifal.edu.br>
2024-10-31 14:04:55 -0300

    adiciona FlatList estática

--- app/index.js
+++ app/index.js
@@ -1,14 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
-import { Button } from 'react-native-paper'
+import { Button, List } from 'react-native-paper'
+
+const Roupa = ({ descricao, valor }) => {
+  return (
+    <List.Item title={descricao} description={valor} />
+  );
+}
 
 const Index = () => {
+  const roupas = [
+    {
+      descricao: "Roupa 1",
+      valor: 50
+    },
+    {
+      descricao: "Roupa 2",
+      valor: 80
+    },
+    {
+      descricao: "Roupa 3",
+      valor: 100
+    }
+  ];
+
   return (
     <View>
-      <Text>Index</Text>
+      <FlatList data={roupas} renderItem={({ item }) => <Roupa descricao={item.descricao} valor={item.valor} />} />
+
       <Link href='/cadastro'>Nova Roupa</Link>
-      
+
       <Link href='/cadastro' asChild>
         <Button mode='contained'>Nova Roupa</Button>
       </Link>
